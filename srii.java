d2e39a9cdcd95751ea3ff2639430a0348402fa4c
sridivya14 <22h51a0551@cmrcet.ac.in>
2024-11-25 21:28:40 +0530

    divya

---
+++ srii.java
@@ -0,0 +1,14 @@
+class prime {
+    public static boolean isPrime(int number) {
+        if (number <= 1) return false;
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int num = 29;
+        System.out.println(num + " is prime? " + isPrime(num));
+    }
+}
